b9de73e2548441d317d0382840be1c138b885964
Oleg Akinin <kicumkicum@gmail.com>
2016-07-10 17:32:29 +0600

    #1 Move code from core (#4)
    * Add gitignore, editorconfig, clusure-linter.flags
    
    * Add changelog
    
    * Add package.json
    
    * Copy code from core
    
    * Implement IAudioService
    
    * Remove trash
    
    * Rename VK to AudioServiceApiVKCOM
    
    * Add work to changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v0.1.0 ()
+
+## Features
+* [#1](https://github.com/clin-player/audioservice-vk-com/issues/1)
+Move code from core
---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+/**
+ * @type {IAudioService}
+ */
+module.exports = {
+	api: require('./lib/api'),
+	dataView: require('./lib/data-view')
+};
